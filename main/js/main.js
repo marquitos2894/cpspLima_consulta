cc54fc0dfa708b4c33108566aa51c8207a4a6f5c
marcos <marcos10junio28@gmail.com>
2019-05-19 15:56:42 -0500

    Detalles de front

--- main/js/main.js
+++ main/js/main.js
@@ -1,5 +1,6 @@
 (function(){
     const server = "localhost"
+    //const server = "www.allcommerceperu.com"
 
     function $(selector){
         return document.querySelector(selector);
@@ -17,8 +18,8 @@
         this.loader = function(visible){
                 var load = $("#load");
             if(visible==true){ 
-                load.style.height="1500px"
-                load.style.width="1500px";
+                load.style.height="100%"
+                load.style.width="100%";
                 load.style.visibility = "visible";
             }else{
                 load.style.height="100x"
@@ -107,21 +108,41 @@
 
                     let template =``;
                     if(data!='undefined'){
+                        
                         render.loader(false);
-                        for(i of  data){
-                            console.log(parseInt(i.Cod_Matricula));
+                        
+                        if(buscarx=="Numero documento"){
+                            console.log(data[0]);
                             template += `
                             <div class="list-group-item list-group-item-action border-primary">           
                                 <div class="d-flex w-100 justify-content-between">
-                                <h5>${i.Paterno} ${i.Materno}, <small class="text-muted">${i.Nombres}</small></h5>
-                                <small>${parseInt(i.Cod_Matricula)}</small>
+                                <h5>${parseInt(data[0].Cod_Matricula)} || ${data[0].Paterno} ${data[0].Materno}, <small class="text-muted">${data[0].Nombres}</small></h5>
+                                <small><a href="#" id="codigo"  data-toggle="modal" data-target=".bd-example-modal-lg"  data-alumno="${data[0].Cod_Alumno}">Ver detalle</a>   </small>
                                 </div>
-                                <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-                                <small>Donec id elit non mi porta.</small>
-                                <a href="#" class="btn btn-primary"id="codigo"  data-toggle="modal" data-target=".bd-example-modal-lg"  data-alumno="${i.Cod_Matricula}">Detalle</a>         
+                               
+                                <strong><p style="color:blue;" class="mb-1">CDR I LIMA Y CALLAO</p></strong>
+                                <small>.......................</small>
+                                        
                             </div>
                             `;
+                        }else{
+                            for(i of  data){
+                                console.log(parseInt(i.Cod_Matricula));
+                                template += `
+                                <div class="list-group-item list-group-item-action border-primary">           
+                                    <div class="d-flex w-100 justify-content-between">
+                                    <h5>${parseInt(i.Cod_Matricula)} || ${i.Paterno} ${i.Materno}, <small class="text-muted">${i.Nombres}</small></h5>
+                                    <small><a href="#" id="codigo"  data-toggle="modal" data-target=".bd-example-modal-lg"  data-alumno="${i.Cod_Alumno}">Ver detalle</a>   </small>
+                                    </div>
+                                   
+                                    <strong><p style="color:blue;" class="mb-1">CDR I LIMA Y CALLAO</p></strong>
+                                    <small>.......................</small>
+                                            
+                                </div>
+                                `;
+                            }
                         }
+                        
                     }else{
                         render.loader(false);
                         template+=`
@@ -134,9 +155,7 @@
                             `;
                     }
                     $("#lista").innerHTML = template;
-            }
-
-                  
+            }    
         }
         
     });
@@ -144,35 +163,81 @@
     $("#lista").addEventListener("click", async function(ev){
         ev.preventDefault();
         if(ev.target.id=="codigo"){
+           
             console.log(ev.target.dataset.alumno);
             let param = ev.target.dataset.alumno;
-            let response = await fetch("http://"+server+"/cpsplima/public/api/codalumno/"+param);
+            //let response = await fetch("http://"+server+"/cpsplima/public/api/codalumno/"+param);
+            let response = await fetch("http://"+server+"/cpsplima/public/api/codalumno_colegiados/"+param);
             let data = await response.json();
             console.log(data);
-            let template = ``;
-            for(i of data){
-            template +=`
-                        <div>
-                            <div class="card profile-card-3">
-                                <div class="background-block">
-                                <h1><span class="badge badge-success">Habilitado</span></h1>
-                                </div>
-                                <div class="profile-thumb-block">
-                                    <img src="main/img/solologo.png" alt="profile-image" class="profile"/>
-                                </div>
-                                <div class="card-content">
-                                    <h2>${i.Paterno} ${i.Materno}, ${i.Nombres} </h2>
+             let template = ``;
+            if(data!='undefined'){
+                for(i of data){
+                    var fecha = new Date(i.VIGENCIA);
+                    dd = fecha.getDate()+1;
+                    mm = fecha.getMonth()+1;
+                    aaaa = fecha.getFullYear();
+                    dd = (dd<10)?'0'+dd:dd;
+                    mm = (mm<10)?'0'+mm:mm;
+
+                    console.log(fecha);
+                    console.log(dd);
+                    console.log(mm);
+                    console.log(aaaa);
+                template +=`
+                            <div>
+                                <div class="card profile-card-3">
+                                    <div class="background-block">
                                     
-                                        <div class="icon-block">
-                                            <div class="alert alert-primary" ><h4><strong><small class="text-muted">Vigencia : </small>30/12/2020</strong></h4></div>
-                                        </div>
-                                        <br>
                                     </div>
-                                </div>
-                                <center><h1><strong><small class="text-muted"><h4>Colegiatura</h4></small><strong><span class="badge badge-primary">${parseInt(i.colegiatura)}</span></h1></center>
+                                    <div class="profile-thumb-block">
+                                        <img src="main/img/solologo.png" alt="profile-image" class="profile"/>
+                                    </div>
+                                    <div class="card-content">
+                                        <h2>${i.Paterno} ${i.Materno}, ${i.Nombres} </h2>
+                                        
+                                            <div class="icon-block">
+                                                
+                                                <h5><strong><p class="mb-1">CDR I LIMA Y CALLAO</p></strong></h5>
+                                                <h5><strong><small class="text-muted">VIGENCIA : </small>${dd}/${mm}/${aaaa}</strong></h5>`;
+                                                if(i.ESTADO=='HABILITADO'){
+                                                    template +=`<h3><span class="badge badge-success">HABILITADO</span></h3>`;
+                                                }else{
+                                                    template +=`<h3><span class="badge badge-danger">NO HABILITADO</span></h3>`;
+                                                }
+                                template +=`</div>
+                                            <br>
+                                        </div>
+                                    </div>
+                                    <center><h1><strong><small class="text-muted"><h4>Colegiatura</h4></small><strong><span class="badge badge-primary">${parseInt(i.Cod_Matricula)}</span></h1></center>
+                            </div>`;   
+                }
+            }else{
+                
+                template +=`
+                <div>
+                    <div class="card profile-card-3">
+                        <div class="background-block">
+                        
                         </div>
-            `;
+                        <div class="profile-thumb-block">
+                            <img src="main/img/solologo.png" alt="profile-image" class="profile"/>
+                        </div>
+                        <div class="card-content">
+                            <h2>No presenta datos</h2>
+                                <div class="icon-block">
+                                    <h5><strong><p class="mb-1">***************</p></strong></h5>
+                                    <h5><strong><small class="text-muted">VIGENCIA : </small>********</strong></h5>
+                                        <h3><span class="badge badge-danger">NO HABILITADO</span></h3>
+                                    </div>
+                                <br>
+                            </div>
+                        </div>
+                        <center><h1><strong><small class="text-muted"><h4>Colegiatura</h4></small><strong><span class="badge badge-primary">******</span></h1></center>
+                </div>`; 
+               
             }
+
             $("#body").innerHTML=template;
             
         }
